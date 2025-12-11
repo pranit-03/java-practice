2db54fa7cc6d8e447a07d46b92dccbfbdb61e779
pranit_03 <pranitduwal@gmail.com>
2025-12-11 10:56:02 +0545

    que 4

--- Lab2/que4.java
+++ Lab2/que4.java
@@ -1,5 +1,16 @@
 package Lab2;
 
+import java.util.Scanner;
+
 public class que4 {
-    
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter an uppercase letter: ");
+        char upper = scanner.next().charAt(0);
+        char lower = Character.toLowerCase(upper);
+        System.out.println("Lowercase: " + lower);
+        scanner.close();
+    }
+
 }
